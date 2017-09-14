75de64ff02a9fba485d8ae7e1b616cb59819f879
zhiyedan <shijia0620@126.com>
2017-09-14 14:33:02 +0800

    add args

--- load.py
+++ load.py
@@ -1,73 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import argparse
 import MySQLdb
 import time
 
-import sys
+def getArgs():
+    parse = argparse.ArgumentParser(prog = "load.py", usage="%(prog)s [options]")
+    parse.add_argument('--host',type=str,help='host')
+    parse.add_argument('-u',type=str,help='user')
+    parse.add_argument('-p',type=str,help='passwd')
+    parse.add_argument('--file',type=str,help='file path')
+
+    args = parse.parse_args()
+    return vars(args)
 
 
-def main(argv):
+def main():
+    args = getArgs()
+    host = args['host']
+    user = args['u']
+    passwd = args['p']
+    file_path = args['file']
 
-start = time.time()
-count = 2
+    if(not host or not user or not passwd or not file_path):
+        print '输入参数有误'
+        return
 
-file_path = '/home/zhiyedan/Desktop/test2'
+    start = time.time()
+    count = 10000
+    insert_field = 'user_id,reg_date,first_order_date,first_order_city,max_order_duration,order_cnt,sum_dist_km,sum_dura_hr,sum_order_day,sum_redpacket,freq_district,max_order_date,max_order_cnt,rushhour_order_cnt'
+    # file_path = '/home/zhiyedan/Desktop/test3'
 
-config = {
-    'host': '127.0.0.1',
-    'port': 3306,
-    'user': 'root',
-    'passwd': 'sj',
-    'db': 'test',
-    'charset': 'utf8'
-}
-conn = MySQLdb.connect(**config)
-conn.autocommit(1)
-cursor = conn.cursor()
+    config = {
+        'host': host,
+        'port': 3306,
+        'user': user,
+        'passwd': passwd,
+        'db': 'test',
+        'charset': 'utf8'
+    }
+    conn = MySQLdb.connect(**config)
+    conn.autocommit(1)
+    cursor = conn.cursor()
+    file = open(file_path,'r')
 
-file = open(file_path,'r')
+    def add_quotes(item,index):
+        for i in index:
+            item[i] = "\'" + item[i] + "\'"
+        return
 
-validData = []
+    round = 0
+    num = 0
+    sub_sql = ''
 
-def add_quotes(item,index):
-    for i in index:
-        item[i] = "\'" + item[i] + "\'"
-    return
+    for line in file:
+        round += 1
+        item = line.split('\t')
+        # if (not item[1].strip() or item[5] == 'NULL' or item[6] == 'NULL'):
+        if (not item[1].strip() or item[1] == 'NULL' or not item[2].strip() or item[2] == 'NULL' or item[5] == 'NULL' or item[6] == 'NULL'):
+            continue
+        add_quotes(item,[0,1,2,3,10,11])
+        # add_quotes(item,[0,2])
+        item_str = ",".join(item)
 
-num = 0
+        sub_sql = sub_sql + '(' + item_str.strip('\r\n') + '),'
+        # print 'sub sql is %s' % sub_sql
+        num += 1
+        if (num == count):
+            sub_sql = sub_sql.strip(',')
+            sql = "insert into mbk_ride17917 ("+ insert_field +") values " + sub_sql
+            # print 'sql is :' + sql
+            cursor.execute(sql)
+            conn.commit()
+            sub_sql = ''
+            num = 0
+            print "lines %d" % round
 
-sub_sql = ''
-
-for line in file:
-    item = line.split('\t')
-    # if (not item[1].strip() or item[5] == 'NULL' or item[6] == 'NULL'):
-    if (not item[1].strip() or item[1] == 'NULL' or not item[2].strip() or item[2] == 'NULL'):
-        continue
-    # add_quotes(item,[0,1,2,3,10,11])
-    add_quotes(item,[0,2])
-    item_str = ",".join(item)
-
-    sub_sql = sub_sql + '(' + item_str.strip('\r\n') + '),'
-    print 'sub sql is %s' % sub_sql
-    num += 1
-    if (num == count):
+    if sub_sql:
         sub_sql = sub_sql.strip(',')
-        sql = "insert into users (name,age,birthday,grade) values " + sub_sql
-        print 'sql is :' + sql
+        sql = "insert into mbk_ride17917 (" + insert_field + ") values " + sub_sql
+        # print 'sql is :' + sql
         cursor.execute(sql)
         conn.commit()
-        sub_sql = ''
-        num = 0
 
-if sub_sql:
-    sub_sql = sub_sql.strip(',')
-    sql = "insert into users (name,age,birthday,grade) values " + sub_sql
-    print 'sql is :' + sql
-    cursor.execute(sql)
-    conn.commit()
-
-cursor.close()
-conn.close()
-end = time.time() - start
-print 'total time is :%d' % end
+    cursor.close()
+    conn.close()
+    end = time.time() - start
+    print 'total time is :%d' % end
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
